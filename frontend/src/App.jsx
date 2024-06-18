b874942b96e49482ab0dd1083e8a8a938dbc6dca
Prince <princevirmani410@gmail.com>
2024-06-18 23:13:20 +0530

    pages and routes setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,19 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
+import Header from "./componets/Header";
+import SignUp from "./pages/SignUp";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="profile" element={<Profile />}></Route>
         <Route path="/sign-in" element={<SignIn />}></Route>
-        <Route path="/sign-up" element={"/"}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
       </Routes>
     </BrowserRouter>
   );
