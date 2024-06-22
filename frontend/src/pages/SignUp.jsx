250b550878940b327d1db5ea357c66fe7b08b686
Prince <princevirmani410@gmail.com>
2024-06-22 06:37:09 +0530

    added redux

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -71,8 +71,9 @@ export default function SignUp() {
       </form>
       <div className="flex gap-2 mt-5">
         <p>Have an account ?</p>
-        <Link to={"/sign-in"}></Link>
-        <span className="text-blue-700">Sign In</span>
+        <Link to={"/sign-in"}>
+          <span className="text-blue-700">Sign In</span>
+        </Link>
       </div>
       {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
---
+++ frontend/src/redux/store.js
@@ -0,0 +1,27 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import userReducer from "./user/userSlice.js";
+import { persistReducer } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import storage from "redux-persist/lib/storage";
+import { version } from "react";
+import persistStore from "redux-persist/es/persistStore";
+
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMidddleware) =>
+    getDefaultMidddleware({
+      serializableCheck: false,
+    }),
+});
+
+export default persistor = persistStore(store);
